72b24d2b01c22a58e2c84eb17cc6e4a175424849
pavel mansilla goyas <pavel.mansilla@gmail.com>
2025-03-01 19:42:19 -0500

    task controller added

---
+++ tasks.js
@@ -0,0 +1,42 @@
+import { readFilePromise, writeFilePromise } from "./fileStream.js";
+
+export async function createTask(taskObj) {
+  let currentTasks = await listTasks();
+  if(currentTasks.length){
+      let lastTask = currentTasks.at(-1)
+      currentTasks.push({...taskObj,id: lastTask.id + 1});
+  }
+  else{
+    currentTasks.push({...taskObj,id:1});
+  }
+  try {
+    await writeFilePromise(JSON.stringify(currentTasks));
+    return { error: false, message: "task created successfully" };
+  } catch (e) {
+    return { error: true, message: e.message };
+  }
+}
+export async function updateTask(taskUpdated) {
+    
+    
+}
+
+export function deleteTask() {}
+
+export async function listTasks() {
+  try {
+    let dataStream = await readFilePromise();
+
+    if(!dataStream){
+        return [];
+    }
+
+    let dataJson = JSON.parse(dataStream);
+    if (!Array.isArray(dataJson)) {
+      throw new Error("json tasks file is not array of task elements");
+    }
+    return dataJson;
+  } catch (e) {
+    console.log(e);
+  }
+}
